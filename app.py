ec0e545ad2d08e3b3ed9a4d9234d2d69617e9fc1
Komal Gacchi <komal.gacchi7788@gmail.com>
2026-06-28 15:50:29 +0530

    Moved db.create_all outside main block for gunicorn deployment

--- app.py
+++ app.py
@@ -334,7 +334,10 @@ def redirect_to_url(short_code):
     db.session.commit()
     return redirect(url.original_url)
 
+# --- APPLICATION START ENGINE ---
+
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
